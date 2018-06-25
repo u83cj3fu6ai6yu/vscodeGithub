eefeeefde726e103eacdd5f615394de476f809e2
Lucien <u83cj3fu6ai6yu@gmail.com>
2018-06-25 16:57:35 +0800

    modity2

--- test1.py
+++ test1.py
@@ -11,33 +11,7 @@ from urllib.parse import urljoin
 import os
 import logging
 
-#from module1 import *
-'''
-# Q2
-myPath = "C:\\Users\\Lucien\\Desktop\\VSC_Python"
-module1.print_dir_contents(myPath)
 
-# Q3
-A0 = dict(zip(('a', 'b', 'c', 'd', 'e'), (1,2,3,4,5)))
-if 'a' in A0:
-    print('OK')
-
-# 裝飾器
-def use_logging(func):
-
-    def wrapper():
-        logging.warn("%s is running" % func.__name__)
-        return func()   # 把 foo 当做参数传递进来时，执行func()就相当于执行foo()
-    
-    return wrapper
-
-@use_logging
-def foo():
-    print('i am foo')
-
-# foo = use_logging(foo) 因為有@語法糖
-foo()                   # 执行foo()就相当于执行 wrapper()
-'''
 
 # class 教學
 class Human(object):
